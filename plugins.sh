1664b397404f172a1e03a953cc8f206f824d9104
pedropcardoso <pedroecomp@gmail.com>
2017-03-14 19:28:26 +0000

    initial commit

---
+++ plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is a shell script"
+sudo apm install atom-bootstrap3 atom-clock atom-typescript autoclose-html autocomplete-html autocomplete-html-entities autocomplete-plus color-picker file-icons
+echo "waiting finish packages"
+sudo apm install language-html linter linter-write-good platformio-ide-terminal
+echo "install themes"
+sudo apm install atom-monokai monokai
